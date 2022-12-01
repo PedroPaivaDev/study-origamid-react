a7d0b59ff788b86fd19ca198ac375e21c6efad34
Pedro <pedropaivadev@gmail.com>
2022-12-01 12:35:41 -0300

    inicio aula208

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
 import React from 'react';
 
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};
-
-function App() {
-  const dados = mario
-
-  const precos = dados.compras.map(item => +item.preco.replace('R$ ', ''))
-  const soma = precos.reduce(function(somaPrecos, preco){
-    return somaPrecos + preco
-  })
-
+const App = () => {
   return (
-    <div >
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>Situação: <span style={{color:dados.ativa ? 'green' : 'red' }}>{dados.ativa ? 'Ativa' : 'Inativa'}</span></p>
-      <p>Total gasto: R$ {soma}</p>
-      {soma > 10000 && <p>Você está gastando muito.</p>}
-    </div>
+    <>
+      {produtos.map(({id, nome, preco, cores}) => {
+        return <section key={id}>
+          <h2>{nome}</h2>
+          <p>{preco}</p>
+          <ul>{cores}</ul>
+        </section>
+      })}
+    </>
   );
-}
+};
 
 export default App;
