5cc1067dfbd401395a823592908ec2df4c2477a5
Pedro <pedropaivadev@gmail.com>
2022-12-09 10:12:59 -0300

    desafio concluído com sucesso!

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React from 'react';
-import InputText from './components/InputText';
-import Radio from './components/Radio';
-import Select from './components/Select';
-import Checkbox from './components/Checkbox';
-import InputCEP from './components/InputCEP';
-import useForm from './hooks/useForm';
-import Input from './components/Input';
+import {perguntas} from './components/perguntas';
 
 const App = () => {
-  const email = useForm('email');
+  const [pergun, setPerguntas] = React.useState(0);
+  const [marcadas, setMarcadas] = React.useState({
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+  });
+  const [respostasCertas, setRespostasCertas] = React.useState(0);
 
-  const [nome, setNome] = React.useState('');
-  const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('');
-  const [fruta, setFruta] = React.useState([]);
-  const [termos, setTermos] = React.useState([]);
-  const [cep, setCep] = React.useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (email.validate()) {
-      console.log('Enviar');
-    } else {
-      console.log('Não enviar');
-    }
+  const renderPergunta = ({pergunta, options, resposta, id}) => {
+    return <div>
+      <section style={{border: '2px solid #d1d5db', padding: '30px', marginBottom: '20px'}}>
+        <h4 style={{backgroundColor: 'white', marginTop: '-45px', boxSizing: 'content-box'}}>{pergunta}</h4>
+        <div>{options.map((option, index) => (
+          <label key={index}>
+            <input 
+              type="radio" 
+              value={option} 
+              onChange={({target}) => (setMarcadas({...marcadas, [id]: target.value}))}
+              checked={marcadas[id] === option}
+            />
+            {option}
+          </label>            
+        ))}</div>
+      </section>
+      <button onClick={(event)=>{
+        event.preventDefault();
+        if (marcadas[id] === '') return;
+        if (marcadas[id] === resposta) setRespostasCertas(respostasCertas+1);
+        setPerguntas(pergun+1);
+      }}>Próxima</button>
+    </div>
   }
+
+  console.log()
   
   return (
-    <form onSubmit={handleSubmit}>
-      <InputText label="Nome" id="nome" type="text" value={nome} setValue={setNome} placeholder="Pedro"/>
-      <Input label="Email" id="email" type="email" {...email} />
-      <h3>Cores</h3>
-      <Radio options={['azul', 'verde', 'amarelo']} value={cor} setValue={setCor}/>
-      <Select options={['Notebook', 'Smartphone', 'Tablet']} value={produto} setValue={setProduto}/>
-      <h3>Frutas</h3>
-      <Checkbox options={['Uva', 'Laranja', 'Limão']} value={fruta} setValue={setFruta}/>
-      <InputCEP label="CEP" id="cep" type="number" value={cep} setValue={setCep} placeholder="00000-000"/>
-      <h3>Termos e condições</h3>
-      <Checkbox options={['Li e aceito os termos e condições']} value={termos} setValue={setTermos}/>
-      <button>Enviar</button>
+    <form>
+      {
+        pergun<Object.keys(perguntas).length ? 
+        <section>{renderPergunta(perguntas[pergun])}</section> :
+        <p>Você acertou: {respostasCertas} de 4 Perguntas</p>
+      }
     </form>
   );
 };
