88c9cd828b7536019cd354f9dd07763abb1f7445
Pedro <pedropaivadev@gmail.com>
2022-12-22 11:16:22 -0300

    style header and icons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import Login from './components/Login/Login';
+import Login from './components/login/Login';
 
 function App() {
   return (
