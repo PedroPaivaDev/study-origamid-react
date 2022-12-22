fde75be951c973494e41ac1a220f7b6f8b5cd793
Pedro <pedropaivadev@gmail.com>
2022-12-22 10:06:15 -0300

    body and css-Reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import Api from './api/Api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div><Api/></div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Login/>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
