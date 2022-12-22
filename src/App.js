060fb2783cc1f49714f293c6563b90f8dc226b1d
Pedro <pedropaivadev@gmail.com>
2022-12-22 09:39:16 -0300

    start final project: dogs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
+import Api from './api/Api';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Produtos from './components/Produtos';
-import Contato from './components/Contato';
-import Produto from './components/Produto';
-import NaoEncontrado from './components/NaoEncontrado';
-import Footer from './components/Footer';
-// import { ProductProvider } from './providers/ProductProvider';
 
-const App = () => {
-  
+function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        {/* <ProductProvider> */}
-          <Header />
-          <div className='content'>
-            <Routes>
-              <Route path='/' element={<Produtos />} />
-              <Route path='contato' element={<Contato />} />
-              <Route path='produto/:id' element={<Produto />} />
-              <Route path='*' element={<NaoEncontrado />} />
-            </Routes>
-          </div>
-          <Footer />
-        {/* </ProductProvider> */}
-      </BrowserRouter>
-    </div>
+    <div><Api/></div>
   );
-};
+}
 
 export default App;
--- src/service/api.js
+++
@@ -1,5 +0,0 @@
-export default async function getProducts(url) {
-    const response = await fetch(url);
-    const json = response.json();
-    return json;
-}
