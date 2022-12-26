88b2c3fbcb76e8b0acdd4a9ca190edf49b8d556b
Pedro <pedropaivadev@gmail.com>
2022-12-26 10:55:51 -0300

    LoginCreate page and fetch post method

--- src/components/Login/LoginCreate.js
+++ src/components/Login/LoginCreate.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import useForm from '../../hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+import {USER_POST} from '../../service/api';
+import {UserContext} from '../../contexts/UserContext';
 
 const LoginCreate = () => {
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  const {userLogin} = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const {url, options} = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
+    if(response.ok) userLogin(username.value, password.value);
+  }
+
   return (
-    <div>LoginCreate</div>
+    <section className='animeLeft'>
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
 
-export default LoginCreate
+export default LoginCreate;
