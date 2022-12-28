96b7caf782a1d6c28c32981b48c1aa3370fa6b53
Pedro <pedropaivadev@gmail.com>
2022-12-28 11:51:18 -0300

    PhotoComments post method and render

--- src/components/Photo/PhotoComments.js
+++ src/components/Photo/PhotoComments.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styles from './PhotoComments.module.css';
+import { UserContext } from '../../contexts/UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+
+  const [comments, setComments] = React.useState(() => props.comments);
+  const {login} = React.useContext(UserContext);
+
   return (
-    <div>PhotoComments</div>
+    <>
+      <ul className={styles.comment}>
+        {comments.map((comment) => (<li key={comment.comment_ID}>
+          <b>{comment.comment_author}: </b>
+          <span>{comment.comment_content}</span>
+        </li>))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+    </>
   )
 }
 
-export default PhotoComments
+export default PhotoComments;
