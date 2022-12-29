56c9bd0f19e37ce71769a37e78130f0a6cb27e24
Pedro <pedropaivadev@gmail.com>
2022-12-29 10:58:46 -0300

    PasswordLost page and post method

--- src/components/Login/LoginPasswordLost.js
+++ src/components/Login/LoginPasswordLost.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../hooks/useForm';
+import useFetch from '../../hooks/useFetch';
+import { PASSWORD_LOST } from '../../service/api';
+import Error from '../Helper/Error';
 
 const LoginPasswordLost = () => {
+  const login = useForm();
+  const {data, loading, error, request} = useFetch();
+
+  const urlLocation = window.location.href.replace('perdeu', 'resetar');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if(login.validate()) {
+      const {url, options} = PASSWORD_LOST({login:login.value, url:urlLocation});
+      const {json} = await request (url, options);
+    }    
+  } /* essa url deve ser a do site que eu fiz o deploy */
+
   return (
-    <div>LoginPasswordLost</div>
+    <section>
+      <h1 className='title'>Perdeu a senha?</h1>
+      {data ? <p style={{color: '#4c1'}}>{data}</p> : 
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="login" {...login} />
+          {loading ? <Button disabled>Enviando...</Button> : <Button>Enviar email</Button>}
+        </form>
+      }
+      <Error error={error}/>
+    </section>
   )
 }
 
-export default LoginPasswordLost
+export default LoginPasswordLost;
